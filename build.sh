4ca3210de7fce4929b7ed3802b62b33a2e1b8d3a
TFNeto <tiago8filipe@gmail.com>
2026-03-02 11:23:13 +0100

    update build for faster build in actions

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ git submodule update
 source poky/oe-init-build-env
 
 CONFLINE="MACHINE = \"qemuarm64\""
-
+echo "$(whoami)"
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
 
@@ -19,8 +19,8 @@ if [ $local_conf_info -ne 0 ];then
 	echo ${CONFLINE} >> conf/local.conf
 
 	#add configs for faster builds and to save space
-	echo "DL_DIR = \"\${HOME}/yocto/build/downloads\" " >> conf/local.conf
-	echo "SSTATE_DIR = \"\${HOME}/yocto/build/sstate-cache\" " >> conf/local.conf
+	echo "DL_DIR = \"/home/tiagoneto/yocto/build/downloads\" " >> conf/local.conf
+	echo "SSTATE_DIR = \"/home/tiagoneto//yocto/build/sstate-cache\" " >> conf/local.conf
 	echo "INHERIT += \"rm_work\"" >> conf/local.conf
 	echo "RM_WORK_EXCLUDE += \"core-image-aesd\"" >> conf/local.conf
 	echo "RM_WORK_EXCLUDE += \"aesd-assignments\"" >> conf/local.conf
